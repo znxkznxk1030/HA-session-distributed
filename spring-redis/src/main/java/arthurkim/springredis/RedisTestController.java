76396bf8430a1915a256c778ee4e0354a996b924
ysKim <znxkznxk1030@gmail.com>
2022-03-30 10:57:21 +0900

    redis session 예제

--- spring-redis/src/main/java/arthurkim/springredis/RedisTestController.java
+++ spring-redis/src/main/java/arthurkim/springredis/RedisTestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 @RequestMapping("/redis")
 public class RedisTestController {
@@ -14,12 +16,23 @@ public class RedisTestController {
     private RedisTestService redisTestService;
 
     @GetMapping("/string")
-    public String getRedisTestService(String key) {
-        return redisTestService.getRedisStringValue(key);
+    public String getRedisTestService(HttpSession session, String key) {
+        return session.getId() + " : " + redisTestService.getRedisStringValue(key);
     }
 
     @PostMapping("/string")
     public void setRedisTestService(String key, String value) {
         redisTestService.setRedisStringValue(key, value);
     }
+
+    @GetMapping("/session")
+    public String getRedisTestService(HttpSession session) {
+        return session.getId() + " : " + session.getAttribute("key-001");
+    }
+
+    @PostMapping("/session")
+    public void setRedisTestService(HttpSession session, String value) {
+        session.setAttribute("key-001", value);
+    }
+
 }
